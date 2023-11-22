fe92f488a632f128fc03adc0fd11febbb503325b
Flickyyy <nasonov.piter@gmail.com>
2023-11-22 23:16:34 +0300

    feat: add a todo list and add and done func

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { randomInt } from 'crypto';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "iwilldoittomorrow" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('iwilldoittomorrow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from IWillDoItTomorrow!');
+	let jokes: string[] = ["глухой слышал, как", "немой расказывал, что", "слепой увидел, как", "хромой быстро-быстро бежал",
+					"Штирлиц стрелял вслепую. Слепая испугалась и побежала скачками, но качки быстро отстали",
+					"Штирлиц всю ночь топил камин. На утро камин утонул",
+					"Штирлиц стоял над картой мира. Его неудержимо рвало на родину",
+					"Штирлиц долго смотрел в одну точку. Потом в другую. \"Двоеточие!\" - наконец-то смекнул Штирлиц"];
+	let disposable = vscode.commands.registerCommand('iwilldoittomorrow.getajoke', () => {
+		vscode.window.showInformationMessage(jokes[randomInt(0, jokes.length)]);
 	});
-
+	//something like a return value
 	context.subscriptions.push(disposable);
+
+	//Anyway i cac do like:
+	//context.subscriptions.push(
+	//	vscode.commands.registerCommand("CommandName", function)
+	//);
+
+	let todo: string[] = [
+		
+	];
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("iwilldoittomorrow.addtodo", async () => {
+			// Get the active text editor
+			const editor = vscode.window.activeTextEditor;
+			//let document = editor.document;
+			//const documentText = document.getText();
+
+			let todo_query = await vscode.window.showInputBox({
+				placeHolder: "Todo query",
+				prompt: "Write a name of your task and some info about that",
+				value: 'Task #'
+			});
+			if (editor) {
+				todo_query += ' [line: ' + String(editor.selection.active.line) + ' file:' + editor.document.fileName + ']';
+			}
+
+			if(todo_query === ''){
+				console.log(todo_query);
+				vscode.window.showErrorMessage('A todo_query is mandatory to execute this action');
+			}
+			
+			if(todo_query !== undefined){
+				todo.push(todo_query);
+				console.log('todo list: ', todo);
+			}
+			
+		})
+	);
+
+
+	
+	context.subscriptions.push(
+		vscode.commands.registerCommand('iwilldoittomorrow.donetodo', async () => {
+			const task = await vscode.window.showQuickPick(todo);
+			if (task !== undefined) {
+				//Check if user really want to delete a task
+				const response = await vscode.window.showInformationMessage("Are you sure you have done this task? ^_^", "YES", "NO");
+				if (response !== undefined && response === 'YES') {
+					console.log(task, "have been done");
+					const index = todo.indexOf(task, 0);
+					if (index > -1) {
+						todo.splice(index, 1);
+					}
+				}
+			}
+		})
+	);
+	
 }
 
-// This method is called when your extension is deactivated
+
 export function deactivate() {}
